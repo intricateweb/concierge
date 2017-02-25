f160115167de04ce25841f4c00aa8a142a4a5015
Woody Mendoza <woody@elderlawanswers.com>
2017-02-25 11:21:45 -0500

    check for user table to prevent error should it already exist.

--- migrations/2016_02_01_000000_create_users_table.php
+++ migrations/2016_02_01_000000_create_users_table.php
@@ -12,13 +12,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('username'); // Long enough as to hold an md5
-            $table->string('name');
-            $table->string('email');
-            $table->nullableTimestamps();
-        });
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('username'); // Long enough as to hold an md5
+                $table->string('name');
+                $table->string('email');
+                $table->nullableTimestamps();
+            });
+        }
     }
 
     /**
